cfa8bdfa88625e3a6ad5443ee529df3fca4354dd
Matt Osborn <matthew.osborn@theguardian.com>
2015-05-15 12:49:41 +0100

    conflicts

--- src/js/components/table.js
+++ src/js/components/table.js
@@ -2,12 +2,18 @@ export default class Table {
     constructor(opts) {
         this.el = opts.el
         this.headers = opts.headers
+        this.caption = opts.caption
         this.opts = opts
         this.renderTable()
+        this.renderCaption(this.caption)
     }
 
     renderTable() {
-        this.el.innerHTML = `<table><thead><tr>${this.headersHTML}</tr></thead><tbody></tbody></table>`
+        this.el.innerHTML = `<table><caption></caption><thead><tr>${this.headersHTML}</tr></thead><tbody></tbody></table>`
+    }
+
+    renderCaption(caption) {
+        this.el.querySelector('caption').innerHTML = caption;
     }
 
     get headersHTML() {
--- src/js/main.js
+++ src/js/main.js
@@ -7,22 +7,14 @@ function init(el, context, config, mediator) {
 
     el.innerHTML = mainHTML
 
-    var headers = ['Rank 2015', "Institution", "Guardian score/100", "Satisfied with course",
-                "Satisfied with teaching", "Student to staff ratio",
-                "Spend per student/10", "Average entry tariff", "Value added score/10", "Career after 6 months"];
-
     var tableComponent = new Table({
         el: el.querySelector('#ug15 .ug15__table-container'),
-        headers: headers
+        caption: 'All universities',
+        headers: ['Rank 2015', "Institution", "Guardian score/100", "Satisfied with course",
+                "Satisfied with teaching", /*"Satisfied with feedback",*/ "Student to staff ratio",
+                "Spend per student/10", "Average entry tariff", "Value added score/10", "Career after 6 months"]
     })
 
-	reqwest({
-	    url: '/data.json',
-	    type: 'json',
-	    crossOrigin: true,
-	    success: (resp) => tableComponent.renderData([[0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0]])
-
-	});
     tableComponent.renderData(data);
 }
 
