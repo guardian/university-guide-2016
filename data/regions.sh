3e79d6189868372b4c0e6512b4d564e5135a9c93
Will Franklin <will.franklin@guardian.co.uk>
2015-06-18 11:59:42 +0100

    Cleanup

--- data/regions.sh
+++ data/regions.sh
@@ -10,7 +10,7 @@ cat institutionDetails.json | jq -r '.[] | .postcode' | tr -d ' ' |
 
 # Join with institution IDs
 cat institutionDetails.json | jq '.[] | .institutionId' |
-    paste -d: - $tmp -d: | tr '\n' ',' | sed 's#^#{#' | sed 's#,$#}#' |
+    paste -d: - $tmp | tr '\n' ',' | sed 's#^#{#' | sed 's#,$#}#' |
     # Map official region names to what we are using
     sed 's#\(East\|West\)"#\1 England"#g' | sed 's#Eastern#East of England#g' > regions.json
 
