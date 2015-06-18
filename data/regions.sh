4cd9032dadc9ac445d40fa2f9b89d85a52b5241a
Will Franklin <will.franklin@guardian.co.uk>
2015-06-18 11:57:24 +0100

    Correct region names

--- data/regions.sh
+++ data/regions.sh
@@ -2,12 +2,16 @@
 
 tmp=$(mktemp)
 
+# Get european region names
 cat institutionDetails.json | jq -r '.[] | .postcode' | tr -d ' ' |
     sed 's#^#http://mapit.mysociety.org/postcode/#' |
     wget -i - -w1 -O - | sed 's#}{#}\n{#' |
     jq '.areas | to_entries[] | select(.value.type == "EUR") | .value.name' > $tmp
 
+# Join with institution IDs
 cat institutionDetails.json | jq '.[] | .institutionId' |
-    paste -d: - $tmp -d: | tr '\n' ',' | sed 's#^#{#' | sed 's#,$#}#' > regions.json
+    paste -d: - $tmp -d: | tr '\n' ',' | sed 's#^#{#' | sed 's#,$#}#' |
+    # Map official region names to what we are using
+    sed 's#\(East\|West\)"#\1 England"#g' | sed 's#Eastern#East of England#g' > regions.json
 
 rm $tmp
